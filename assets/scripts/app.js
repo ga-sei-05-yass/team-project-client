7b605bac5bc38de8c61bb853207e70eccbe76094
yingsongsana <yingsongsana@gmail.com>
2019-11-05 14:38:41 -0500

    Created ajax requests in new api file
    (Really hide the div element this time in app file. Originally mentioned
    in previous commit, but was not logged with the commit)

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,4 @@
 'use strict'
-
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
@@ -7,5 +6,6 @@
 // require('./example')
 
 $(() => {
-  // your JS code goes here
+  // hides ui alert messages
+  $('.alert').hide()
 })
